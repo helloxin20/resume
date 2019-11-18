4622b3a28a6e0f846c776c95aae46299685f2587
helloxin20 <525398069@qq.com>
2019-11-18 11:31:58 +0800

    'fix:修改配置'

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <template>
   <div id="app">
     <router-view></router-view>
+    <button id="btn">打印pdf</button>
   </div>
 </template>
 
 <script>
+
 export default {
   name: 'app',
   mounted() {
@@ -13,6 +15,11 @@ export default {
      */
     const { NODE_ENV } = process.env;
     document.documentElement.className = NODE_ENV;
+    /*const html2pdf =  require("@/plugin/html2pdf.js-0.9.1/dist/html2pdf.bundle.js");
+    var element = document.getElementById('app');*/
+    /*#btn.click(){
+      html2pdf(element);
+    }*/
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -4,8 +4,8 @@ import App from './App.vue'
 import router from './router'
 Vue.config.productionTip = false;
 
-import pdf from 'html2pdf.js'
-Vue.use(pdf);
+/*import html2pdf from 'html2pdf.js'
+Vue.use(html2pdf);*/
 
 new Vue({
   render: h => h(App),
